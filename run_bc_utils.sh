127e44a57ef9ffd56a8dfde0045d73f5c8511ec9
makutaku <14153280+makutaku@users.noreply.github.com>
2024-06-04 22:39:40 -0500

    Echoing $BCU_OUTPUT_DIR before running bc_utils.py

--- run_bc_utils.sh
+++ run_bc_utils.sh
@@ -1,5 +1,6 @@
 . "$BCU_REPO_DIR/bcutils_env/bin/activate" && \
   cd "$BCU_REPO_DIR/bcutils" && \
+  echo "BCU_OUTPUT_DIR=$BCU_OUTPUT_DIR" && \
   python bc_utils.py #2>&1 | tee -a ./bc_utils.log
 
 
