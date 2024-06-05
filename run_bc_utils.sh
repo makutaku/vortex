0403901afa852d0be3adc7183189dc9385707b81
makutaku <14153280+makutaku@users.noreply.github.com>
2024-06-04 23:04:02 -0500

    Sourcing the container.env vars in the wrapper.

--- run_bc_utils.sh
+++ run_bc_utils.sh
@@ -1,6 +1,13 @@
+# Load environment variables from .env file
+set -a # Automatically export all variables
+source $BCU_REPO_DIR/container.env
+set +a
+
+set
+
 . "$BCU_REPO_DIR/bcutils_env/bin/activate" && \
   cd "$BCU_REPO_DIR/bcutils" && \
-  echo "BCU_OUTPUT_DIR=$BCU_OUTPUT_DIR" && \
+  echo "BCU_USERNAME=$BCU_USERNAME" && \
   python bc_utils.py #2>&1 | tee -a ./bc_utils.log
 
 
